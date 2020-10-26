e3c822491855468393f83ec9083654fce9a093cf
Arienlys <48921432+Arienlys@users.noreply.github.com>
2020-10-26 11:03:54 +0100

    [main-banner] giving the span it's own line
    Co-authored-by: Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>

--- src/components/main-banner.vue
+++ src/components/main-banner.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="inner">
-        <div class="logo"><span class="icon fa-gem"></span></div>
+        <div class="logo">
+          <span class="icon fa-gem"></span>
+        </div>
         <h2>This is Solid State</h2>
         <p>
             Another free + fully responsive site template by
